b1c6ad3a709539c546cec2a31466a36c5a438b66
Ecuki <31224521+Ecuki@users.noreply.github.com>
2020-05-06 15:22:51 +0200

    add tracking utils/config

---
+++ utils/config.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+let PORT = process.env.PORT;
+let MONGODB = process.env.MONGODB;
+
+if (process.env.NODE_ENV === "test") {
+  MONGODB = process.env.TEST_MONGODB;
+}
+
+module.exports = {
+  MONGODB,
+  PORT,
+};
